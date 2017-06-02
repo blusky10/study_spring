233a409bc8301a8a121c2dd09fd992b6a59abdd8
kimsanghyun <sanghyun0910.kim@samsung.com>
2017-06-02 09:42:29 +0900

    CREATE: spring study 프로젝트 생성

---
+++ src/main/java/com/study/srping/StudySpringApplication.java
@@ -0,0 +1,12 @@
+package com.study.srping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpringApplication.class, args);
+	}
+}
