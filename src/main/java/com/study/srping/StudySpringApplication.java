c3ce4be6d9c5d3bc9dce985765b9b1c78f3a491f
kimsanghyun <sanghyun0910.kim@samsung.com>
2017-06-06 07:43:40 +0900

    CREATE : banner 파일 작성

--- src/main/java/com/study/srping/StudySpringApplication.java
+++ src/main/java/com/study/srping/StudySpringApplication.java
@@ -1,12 +1,29 @@
 package com.study.srping;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.io.PrintStream;
 
 @SpringBootApplication
 public class StudySpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudySpringApplication.class, args);
+		SpringApplication app = new SpringApplication(StudySpringApplication.class);
+
+		/**
+		 * Boot Application 구동시 배너 설정
+		 * banner.txt 파일을 클래스 패스에 넣어도 동작한다.
+		 */
+		app.setBanner(new Banner() {
+			@Override
+			public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
+				out.print("이거다");
+			}
+		});
+		app.run(args);
+
 	}
 }
