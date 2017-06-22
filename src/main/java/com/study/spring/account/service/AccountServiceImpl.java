1bf28ea56cc3fc18eba103ab41320aa37332e2c3
kimsanghyun <sanghyun0910.kim@samsung.com>
2017-06-22 09:22:12 +0900

    UPDATE : 도메인 생성

--- src/main/java/com/study/spring/account/service/AccountServiceImpl.java
+++ src/main/java/com/study/spring/account/service/AccountServiceImpl.java
@@ -3,10 +3,12 @@ package com.study.spring.account.service;
 import com.study.spring.account.repository.AccountRepository;
 import com.study.spring.domain.Account;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by SDS on 2017-06-21.
  */
+@Service
 public class AccountServiceImpl implements AccountService {
 
     @Autowired
