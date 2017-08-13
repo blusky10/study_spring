e4638d724cd5d84e31466040a4b3048f9a675321
kimsanghyun <sanghyun0910.kim@samsung.com>
2017-08-13 15:02:07 +0900

    udpate

--- src/main/java/com/study/spring/domain/Operation.java
+++ src/main/java/com/study/spring/domain/Operation.java
@@ -3,17 +3,18 @@ package com.study.spring.domain;
 import javax.persistence.*;
 
 /**
- INSERT INTO `operation` (`id`, `name`, `possible_to_create`, `possible_to_read`, `possible_to_update`, `possible_to_delete`) VALUES
- (1001, 'CREATE', 1, 0, 0, 0),
- (1002, 'READ', 0, 1, 0, 0),
- (1003, 'UPDATE', 0, 0, 1, 0),
- (1004, 'DELETE', 0, 0, 0, 1),
- (1005, 'CREATE&READ', 1, 1, 0, 0),
- (1006, 'CREATE&UPDATE', 1, 0, 1, 0),
- (1007, 'CREATE&DELETE', 1, 0, 0, 1),
- (1008, 'CREATE&READ&UPDATE', 1, 1, 1, 0),
- (1009, 'CREATE&UPDATE&DELETE', 1, 0, 1, 1),
- (1010, 'CREATE&READ&UPDATE&DELETE', 1, 1, 1, 1);
+ INSERT INTO `sp_operation` (`operation_uid`, `operation_name`, `operation_create`, `operation_read`, `operation_update`, `operation_delete`, `operation_click`, `operation_enable`) VALUES
+ (1001, 'CREATE', 							1, 0, 0, 0, 0, 1),
+ (1002, 'READ',								0, 1, 0, 0, 0, 1),
+ (1003, 'UPDATE', 							0, 0, 1, 0, 0, 1),
+ (1004, 'DELETE', 							0, 0, 0, 1, 0, 1),
+ (1005, 'CREATE&READ', 						1, 1, 0, 0, 0, 1),
+ (1006, 'CREATE&UPDATE', 					1, 0, 1, 0, 0, 1),
+ (1007, 'CREATE&DELETE', 					1, 0, 0, 1, 0, 1),
+ (1008, 'CREATE&READ&UPDATE', 			1, 1, 1, 0, 0, 1),
+ (1009, 'CREATE&UPDATE&DELETE', 			1, 0, 1, 1, 0, 1),
+ (1010, 'CREATE&READ&UPDATE&DELETE', 	1, 1, 1, 1, 0, 1),
+ (1011, 'CLICK', 								0, 0, 0, 0, 1, 1);
  */
 
 @Entity
