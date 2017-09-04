62bede39d0cc39951d127e77d656edd871975399
kimsanghyun <sanghyun0910.kim@samsung.com>
2017-09-04 09:24:05 +0900

    update:initdata

--- src/main/resources/initdata.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `account` (`loing_id`, `email`, `enable`, `password`, `username`) VALUES
-	('admin', 'admin@test.com', b'1', 'admin1!', 'admin');
-
-INSERT INTO `role` (`id`, `description`, `enable`, `name`) VALUES
-	(1, NULL, b'1', 'CLIENT');
-
-INSERT INTO `account_roles` (`account_loing_id`, `roles_id`) VALUES
-	('admin', 1);
