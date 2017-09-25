0b49a55392747f6aa62a9a5197f84577ec80c809
blusky10 <blusky0910@gmail.com>
2017-09-25 23:33:07 +0900

    update:login 수정

--- src/main/java/com/study/spring/main/web/MainController.java
+++ src/main/java/com/study/spring/main/web/MainController.java
@@ -1,28 +1,44 @@
 package com.study.spring.main.web;
 
+import com.study.spring.account.repository.AccountRepository;
+import com.study.spring.domain.Account;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class MainController {
 
     private static final String VIEW_LOGIN = "login";
 
-    @RequestMapping(value = "/")
-    public String index(){
-        return "home";
+    @Autowired
+    private AccountRepository accoutRepository;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView ingex(ModelAndView modelAndView){
+        List<Account> accounts = accoutRepository.findAll();
+        modelAndView.setViewName("index");
+        modelAndView.addObject("accounts", accounts);
+
+        return modelAndView;
     }
 
     @RequestMapping(value = "/login")
-    public String login(){
-        return VIEW_LOGIN;
+    public ModelAndView login(ModelAndView modelAndView){
+        modelAndView.setViewName(VIEW_LOGIN);
+        return modelAndView;
     }
 
-    @RequestMapping(value = "/home")
-    public String home(){
-        return "home";
-    }
+//    @RequestMapping(value = "/home")
+//    public String home(){
+//        return "home";
+//    }
 
     @RequestMapping(value = "/private")
     public String privateApi(){
