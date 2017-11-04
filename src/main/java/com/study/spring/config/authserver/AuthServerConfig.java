6bcbc1dc477fc05c2112e0c6c537235a3bcb6f96
kimsanghyun <sanghyun0910.kim@samsung.com>
2017-11-04 15:51:22 +0900

    update

--- src/main/java/com/study/spring/config/authserver/AuthServerConfig.java
+++ src/main/java/com/study/spring/config/authserver/AuthServerConfig.java
@@ -1,8 +1,6 @@
 package com.study.spring.config.authserver;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -10,10 +8,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableAuthorizationServer
@@ -22,9 +16,6 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
     @Autowired
     private AuthenticationManager authenticationManager;
 
-    @Autowired
-    private DataSource dataSource;
-
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
         security.checkTokenAccess("isAuthenticated()");
@@ -32,24 +23,37 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.jdbc(dataSource);
-//        clients.inMemory().withClient("myclient")
-//                .authorizedGrantTypes("client_credentials", "password")
-//                .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
-//                .scopes("read", "write", "trust")
-//                .resourceIds("oauth2-resource")
-//                .accessTokenValiditySeconds(500)
-//                .secret("secret");
-
+        clients.inMemory().withClient("myclient")
+                .authorizedGrantTypes("client_credentials", "password")
+                .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
+                .scopes("read", "write", "trust")
+                .resourceIds("oauth2-resource")
+                .accessTokenValiditySeconds(500)
+                .secret("secret");
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.tokenStore(tokenStore()).authenticationManager(authenticationManager);
+        endpoints.authenticationManager(authenticationManager);
     }
 
-    @Bean
-    public TokenStore tokenStore(){
-        return new JdbcTokenStore(dataSource);
-    }
+//    [Client 정보를 DB 에서 가져올 경우 Setting]
+//    @Autowired
+//    private DataSource dataSource;
+//    @Bean
+//    public TokenStore tokenStore(){
+//        return new JdbcTokenStore(dataSource);
+//    }
+//    @Override
+//    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+//        clients.jdbc(dataSource);
+//    }
+//    @Override
+//    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+//        endpoints.tokenStore(tokenStore()).authenticationManager(authenticationManager);
+//    }
+//    @Bean
+//    public TokenStore tokenStore(){
+//        return new JdbcTokenStore(dataSource);
+//    }
 }
