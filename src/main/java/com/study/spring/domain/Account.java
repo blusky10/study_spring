fb4b3b569ceef044361e8cd21fce5d94854204ab
blusky10 <blusky0910@gmail.com>
2017-12-11 22:33:02 +0900

    lombok

--- src/main/java/com/study/spring/domain/Account.java
+++ src/main/java/com/study/spring/domain/Account.java
@@ -1,10 +1,13 @@
 package com.study.spring.domain;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Data
 public class Account {
 
     @Id
@@ -24,52 +27,4 @@ public class Account {
 
     @OneToMany
     private List<Role> roles;
-
-    public String getLoginId() {
-        return loginId;
-    }
-
-    public void setLoginId(String loginId) {
-        this.loginId = loginId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public boolean isEnable() {
-        return enable;
-    }
-
-    public void setEnable(boolean enable) {
-        this.enable = enable;
-    }
-
-    public List<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(List<Role> roles) {
-        this.roles = roles;
-    }
 }
--- src/main/java/com/study/spring/domain/Role.java
+++ src/main/java/com/study/spring/domain/Role.java
@@ -1,10 +1,11 @@
 package com.study.spring.domain;
 
+import lombok.Data;
+
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
+@Data
 public class Role {
 
     @Id
@@ -18,36 +19,4 @@ public class Role {
 
     @Column(nullable = false)
     private boolean enable;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public boolean isEnable() {
-        return enable;
-    }
-
-    public void setEnable(boolean enable) {
-        this.enable = enable;
-    }
 }
