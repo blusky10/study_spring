e62bd369973df5f0b709189d026e72a159154721
kimsanghyun <sanghyun0910.kim@samsung.com>
2017-12-19 10:33:46 +0900

    import sql 수정

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,5 @@
-INSERT INTO account (id, email, enable, login_id, password, username)
-VALUES  (1, 'admin@spring.com', 1, 'admin', 'admin1!', 'admin user');
+INSERT INTO account (id, email, enable, login_id, password, username) VALUES  (1, 'admin@spring.com', 1, 'admin', 'admin1!', 'admin user');
 
-INSERT INTO role (id, description, enable, name)
-VALUES  (1, NULL, 1, 'ADMIN'),
-  (2, NULL, 1, 'GUEST');
+INSERT INTO role (id, description, enable, name) VALUES  (1, NULL, 1, 'ADMIN'),   (2, NULL, 1, 'GUEST');
 
 INSERT INTO account_roles (account_id, roles_id) VALUES (1, 1);
