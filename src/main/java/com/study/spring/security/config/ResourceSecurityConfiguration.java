876c7e51c1308538afab9131dee0ab0945e82198
blusky0910@gmail.com <blusky0910@gmail.com>
2019-08-30 11:04:47 +0900

    수정: 옵션 추가 및 h2-console 추가

--- src/main/java/com/study/spring/security/config/ResourceSecurityConfiguration.java
+++ src/main/java/com/study/spring/security/config/ResourceSecurityConfiguration.java
@@ -3,36 +3,33 @@ package com.study.spring.security.config;
 
 import com.study.spring.security.handler.LoginFailureHandler;
 import com.study.spring.security.handler.LoginSuccessHandler;
-import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.session.SessionRegistry;
-import org.springframework.security.core.session.SessionRegistryImpl;
-import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 @Configuration
 public class ResourceSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
         http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/", "/login/**","/browser/**", "/error/**", "/oauth/**", "/token/**", "/googlemap/**").permitAll()
-                .antMatchers("/private/**").authenticated()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .successHandler(new LoginSuccessHandler())
-                .failureHandler(new LoginFailureHandler())
-                .and()
-                .logout().permitAll()
-                .and()
-                .sessionManagement()
-                .maximumSessions(1)
-                .maxSessionsPreventsLogin(true);
+            .authorizeRequests()
+            .antMatchers("/", "/login/**","/browser/**", "/error/**", "/oauth/**", "/token/**", "/googlemap/**", "/h2-console/**").permitAll()
+            .antMatchers("/private/**").authenticated()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin()
+            .successHandler(new LoginSuccessHandler())
+            .failureHandler(new LoginFailureHandler())
+            .and()
+            .logout().permitAll()
+            .and()
+            .sessionManagement()
+            .maximumSessions(1)
+            .maxSessionsPreventsLogin(false);
     }
 
     //    @Autowired
