2b888656774d180f635672b0f9f353d8abd0d25e
KIMSANGHYUN <blusky0910@gmail.com>
2019-10-24 08:37:12 +0900

    NoArgsConstructor 어노테이션 추가

--- src/main/java/com/study/spring/account/dto/AccountReqDto.java
+++ src/main/java/com/study/spring/account/dto/AccountReqDto.java
@@ -2,8 +2,10 @@ package com.study.spring.account.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class AccountReqDto {
 
     private String loginId;
