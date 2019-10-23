9c95e48aa837d6d257089ebac1e2d82f372e06a5
blusky10 <blusky0910@gmail.com>
2019-10-23 23:49:34 +0900

    소스 수정:

--- src/main/java/com/study/spring/account/dto/AccountReqDto.java
+++ src/main/java/com/study/spring/account/dto/AccountReqDto.java
@@ -2,10 +2,8 @@ package com.study.spring.account.dto;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 public class AccountReqDto {
 
     private String loginId;
--- src/test/java/com/study/spring/account/web/AccountRestControllerTest.java
+++ src/test/java/com/study/spring/account/web/AccountRestControllerTest.java
@@ -6,6 +6,7 @@ import com.study.spring.account.dto.AccountReqDto;
 import com.study.spring.account.dto.AccountResDto;
 import com.study.spring.account.repository.AccountRepository;
 import com.study.spring.account.service.AccountService;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,24 +14,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.MvcResult;
 
-import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(AccountRestController.class)
 public class AccountRestControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @Autowired
     private AccountRestController accountRestController;
 
     @MockBean
@@ -59,14 +58,16 @@ public class AccountRestControllerTest {
 
     @Test
     public void getResDtoTest() throws Exception {
-
-        given(this.accountRestController.getResDto(reqDto)).willReturn(ResponseEntity.ok(resDto));
-
-        ResultActions resultActions = this.mockMvc.perform(
+        MvcResult mvcResult = this.mockMvc.perform(
                 post("/api/v1/accounts")
-                .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .content(objectMapper.writeValueAsString(reqDto)))
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(objectMapper.writeValueAsString(reqDto)))
                 .andExpect(status().isOk())
-                .andDo(print());
+                .andDo(print())
+                .andReturn();
+
+        String contentAsString = mvcResult.getResponse().getContentAsString();
+        AccountResDto accountResDto = objectMapper.readValue(contentAsString, AccountResDto.class);
+        Assert.assertEquals("test", accountResDto.getLoginId());
     }
 }
