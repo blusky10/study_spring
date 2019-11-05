52e8f875b24a972fd819c5be031120879f6a395b
KIMSANGHYUN <blusky0910@gmail.com>
2019-11-05 16:05:25 +0900

    수정 : account create 메소드 추가

--- src/main/java/com/study/spring/account/web/AccountRestController.java
+++ src/main/java/com/study/spring/account/web/AccountRestController.java
@@ -1,16 +1,14 @@
 package com.study.spring.account.web;
 
+import com.study.spring.account.dto.AccountCreateDto;
 import com.study.spring.account.dto.AccountReqDto;
 import com.study.spring.account.dto.AccountResDto;
-
-
 import com.study.spring.account.service.AccountService;
-import com.study.spring.domain.Account;
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,13 +20,14 @@ public class AccountRestController {
     private AccountService accountService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<Account> get(@PathVariable Long id){
-        return ResponseEntity.ok(accountService.findAccountById(id));
+    public ResponseEntity<AccountResDto> get(@PathVariable Long id){
+        return ResponseEntity.ok(accountService.findAccountById(id).convertToAccountResDto());
     }
 
+
+    // Post 로 조회를 테스트 해보기 위함 삭제 예정
     @PostMapping
     public ResponseEntity<AccountResDto> getResDto(@RequestBody AccountReqDto dto){
-
         log.debug("INPUT : " + dto.toString() );
         return ResponseEntity.ok(AccountResDto.builder().email("test@test.com")
                 .loginId("test")
@@ -40,5 +39,15 @@ public class AccountRestController {
     @GetMapping
     public Page<AccountResDto> getResDto(Pageable pageable){
         return accountService.findAll(pageable);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createAccount(@RequestBody AccountCreateDto accountCreateDto){
+
+        log.debug("ACCOUNT CREATE : " + accountCreateDto.toString());
+
+        accountService.create(accountCreateDto);
+
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 }
