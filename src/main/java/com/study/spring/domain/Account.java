335644334ae0a410a4dfcc78786c633aa74a13ee
KIMSANGHYUN <blusky0910@gmail.com>
2019-11-05 16:04:15 +0900

    수정 : builder, convert 메소드 추가 , role 은 주석 처리

--- src/main/java/com/study/spring/domain/Account.java
+++ src/main/java/com/study/spring/domain/Account.java
@@ -1,19 +1,16 @@
 package com.study.spring.domain;
 
+import com.study.spring.account.dto.AccountResDto;
 import com.study.spring.enums.EnableStatus;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Entity
 @EqualsAndHashCode(callSuper=false)
 //@Audited
 //@AuditOverride(forClass = AbstractAuditable.class)
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Setter
 public class Account {
@@ -38,8 +35,25 @@ public class Account {
     @Enumerated(EnumType.ORDINAL)
     private EnableStatus enable;
 
-    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinColumn(name = "account_id")
-    private List<Role> roles;
+//    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+//    @JoinColumn(name = "account_id")
+//    private List<Role> roles;
+
+    @Builder
+    public Account(String loginId, String username, String password, String email, EnableStatus enable) {
+        this.loginId = loginId;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.enable = enable;
+    }
+
+    public AccountResDto convertToAccountResDto(){
+        return AccountResDto.builder()
+                .username(this.username)
+                .email(this.email)
+                .loginId(this.loginId)
+                .build();
+    }
 
 }
