4672832690f0eadbb3a6d8e262fac5ba8bb6a448
KIMSANGHYUN <blusky0910@gmail.com>
2019-11-05 14:17:16 +0900

    수정 : Account 도메인 객체로 변경하는 메소드 생성

--- src/main/java/com/study/spring/account/dto/AccountCreateDto.java
+++ src/main/java/com/study/spring/account/dto/AccountCreateDto.java
@@ -1,9 +1,13 @@
 package com.study.spring.account.dto;
 
+import com.study.spring.domain.Account;
+import com.study.spring.enums.EnableStatus;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.Arrays;
 
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -17,4 +21,13 @@ public class AccountCreateDto {
     private String password;
 
     private String email;
+
+    public Account convertToAccount(){
+        return Account.builder()
+                .email(this.email)
+                .loginId(this.loginId)
+                .password(this.password)
+                .enable(EnableStatus.TRUE)
+                .build();
+    }
 }
