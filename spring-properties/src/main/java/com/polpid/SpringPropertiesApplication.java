785d6fb47c1ffac6412a4ca65368baf7fd8d4337
sanghyunkim <blusky0910@gmail.com>
2020-06-02 23:20:22 +0900

    log 변경

--- spring-properties/src/main/java/com/polpid/SpringPropertiesApplication.java
+++ spring-properties/src/main/java/com/polpid/SpringPropertiesApplication.java
@@ -1,35 +1,35 @@
 package com.polpid;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@Slf4j
 public class SpringPropertiesApplication {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(SpringPropertiesApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringPropertiesApplication.class, args);
     }
 
-    @Autowired
-    Environment enviroment;
+    private final Environment environment;
+    private final MyProperties myProperties;
 
-    @Autowired
-    private MyProperties myProperties;
+    public SpringPropertiesApplication(Environment environment, MyProperties myProperties) {
+        this.environment = environment;
+        this.myProperties = myProperties;
+    }
 
     @Bean
     CommandLineRunner myRunner(){
         return args -> {
-            LOGGER.info("application Name : " + myProperties.getApplicationName());
-            LOGGER.info("server port : " + enviroment.getProperty("server.port"));
+            log.info("application Name : " + myProperties.getApplicationName());
+            log.info("server port : " + environment.getProperty("server.port"));
             System.out.println();
         };
     }
