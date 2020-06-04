18eeb8701d8a1e20fb8f9185db476e9e10c2949e
KIMSANGHYUN <blusky0910@gmail.com>
2020-06-04 10:06:59 +0900

    라이브러리 추가 & 클래스 이름 변경

--- spring-oauth/src/main/java/com/polpid/SpringOauthServerApplication.java
+++ spring-oauth/src/main/java/com/polpid/SpringOauthServerApplication.java
@@ -1,13 +1,8 @@
 package com.polpid;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @Slf4j
