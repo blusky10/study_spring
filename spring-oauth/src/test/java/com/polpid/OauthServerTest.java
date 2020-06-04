90d907769ab15c226f53e9d0846dc6f775f08973
sanghyunkim <blusky0910@gmail.com>
2020-06-04 23:21:14 +0900

    Object Mapper 추가

--- spring-oauth/src/test/java/com/polpid/OauthServerTest.java
+++ spring-oauth/src/test/java/com/polpid/OauthServerTest.java
@@ -1,5 +1,7 @@
 package com.polpid;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +12,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
+import java.io.IOException;
 import java.util.Base64;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -19,8 +23,11 @@ public class OauthServerTest {
     @Autowired
     private TestRestTemplate testRestTemplate;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
-    public void getAccessToken(){
+    public void getAccessToken() throws IOException {
 
         String basicAuth = "myclient:secret";
         Base64.Encoder encoder = Base64.getEncoder();
@@ -38,6 +45,9 @@ public class OauthServerTest {
 
         ResponseEntity<String> exchange = testRestTemplate.exchange("/oauth/token", HttpMethod.POST, httpEntity, String.class);
 
+        Map map = objectMapper.readValue(exchange.getBody(), Map.class);
+
+        Assert.assertNotNull(map.get("access_token"));
         System.out.println(exchange.getBody());
     }
 }
