d36a68b074da0363732ac9aa361acccbd0046123
KIMSANGHYUN <blusky0910@gmail.com>
2020-06-05 12:04:21 +0900

    Authentication Manager 설정 추가

--- spring-oauth/src/main/java/com/polpid/config/AuthServerConfig.java
+++ spring-oauth/src/main/java/com/polpid/config/AuthServerConfig.java
@@ -1,22 +1,25 @@
 package com.polpid.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
-//    @Autowired
-//    private AuthenticationManager authenticationManager;
+
+    private final AuthenticationManager authenticationManager;
 
     private final PasswordEncoder passwordEncoder;
 
-    public AuthServerConfig(PasswordEncoder passwordEncoder) {
+    public AuthServerConfig(AuthenticationManager authenticationManager, PasswordEncoder passwordEncoder) {
+        this.authenticationManager = authenticationManager;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -27,7 +30,8 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory().withClient("myclient")
+        clients.inMemory()
+                .withClient("myclient")
                 .authorizedGrantTypes("client_credentials", "password")
 //                .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
                 .scopes("read", "write", "trust")
@@ -36,10 +40,10 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
                 .secret(passwordEncoder.encode("secret"));
     }
 
-//    @Override
-//    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-//        endpoints.authenticationManager(authenticationManager);
-//    }
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
 
 //  [JWT Token 사용]===================
 //    @Bean
