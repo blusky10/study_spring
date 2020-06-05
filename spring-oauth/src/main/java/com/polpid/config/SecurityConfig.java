376d15e66e33a6566319413ab85690391f8ade53
KIMSANGHYUN <blusky0910@gmail.com>
2020-06-05 12:03:53 +0900

    AuthenticationManager 추가

--- spring-oauth/src/main/java/com/polpid/config/SecurityConfig.java
+++ spring-oauth/src/main/java/com/polpid/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.polpid.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -13,5 +15,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password(passwordEncoder().encode("admin1@")).roles("USER");
     }
 }
