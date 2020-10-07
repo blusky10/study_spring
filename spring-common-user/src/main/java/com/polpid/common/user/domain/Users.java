8430a41da0317eaabe5baa262d489ceb1b502cdb
KIMSANGHYUN <blusky0910@gmail.com>
2020-10-07 17:10:00 +0900

    User 도메인 수정

--- spring-common-user/src/main/java/com/polpid/common/user/domain/Users.java
+++ spring-common-user/src/main/java/com/polpid/common/user/domain/Users.java
@@ -3,19 +3,30 @@ package com.polpid.common.user.domain;
 import com.polpid.common.user.UserStatus;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.ToString;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+@NoArgsConstructor
 @Getter
-@ToString
 public class Users {
+    @Id
+    @Column(nullable = false,length = 100)
     private String email;
+    @Column(nullable = false,length = 100)
     private String name;
-    private UserStatus status;
+    @Column(nullable = false)
+    private UserStatus userStatus;
 
     @Builder
     public Users(String email, String name, UserStatus status) {
         this.email = email;
         this.name = name;
-        this.status = status;
+        this.userStatus = status;
     }
 }
